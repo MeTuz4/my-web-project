72391993d5a273d1f27ab8c81a1d66383fdd2967
MeTuza <druzin_andrey@mail.ru>
2021-12-15 11:19:17 +0300

    Using Bootstrap v0.88

--- add to table.js
+++ add to table.js
@@ -1,47 +1,19 @@
+function saveChanges(){
+
+}
+
 function onSubmitFun(){
     var tableRow = document.createElement("tr");
     var singer = document.createElement("td");
-    singer.textContent = document.form.form_singer;
+    singer.textContent = document.getElementById("singer").value;
     tableRow.appendChild(singer);
-
     var group = document.createElement("td");
+    group.textContent = document.getElementById("group").value;
     tableRow.appendChild(group);
-
-    // var song = document.createElement("td");
-    // song.textContent = document.form.form_song;
-    // tableRow.appendChild(song);
-
-    var printBlock = document.getElementsByClassName("page-load-timer");
-    printBlock[0].appendChild(timeBlock);
-
-
-    deleteBtn.addEventListener("click", (event) => {
-        deleteBtn.parentElement.remove();
-    });
-    // var val = singer.value;
-    // singer.value = "";
-    // if (val.length > 0){
-    //     const tmpElem = document.getElementById("main-todo-table-tbody__tmpl");
-    //     var clone = tmpElem.content.cloneNode(true);
-    //     clone.querySelectorAll("td")[0].textContent = val;
-    //
-    //     var printBlock = document.getElementById("tbody-lab5");
-    //     printBlock.appendChild(clone);
-    // }
-
-
-
-    // var group = document.form.form_group;
-    //
-    //
-    //
-    // if (val.length > 0){
-    //     const tmpElem = document.getElementById("main-todo-table-tbody__tmpl");
-    //     var clone = tmpElem.content.cloneNode(true);
-    //     clone.querySelectorAll("td")[0].textContent = val;
-    //
-    //     var printBlock = document.getElementById("tbody-lab5");
-    //     printBlock.appendChild(clone);
-    // }
+    var song = document.createElement("td");
+    song.textContent = document.getElementById("song").value;
+    tableRow.appendChild(song);
+    var printBlock = document.getElementById("music_table");
+    printBlock.appendChild(tableRow);
     saveChanges();
 }
